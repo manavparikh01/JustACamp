ba63b29419275923e9fd6f539fa81dc456b72642
manavparikh01 <manavparikh01@gmail.com>
2021-07-14 03:39:33 +0530

    Basic Stuff

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const path = require("path")
 const mongoose = require("mongoose")
 const Campground = require('./models/campground')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressErrors = require('./utils/expressErrors')
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
+const Review = require('./models/review')
 const methodOverride = require("method-override")
+const { resolveSoa } = require("dns")
+const review = require("./models/review")
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
@@ -23,55 +29,107 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressErrors(msg, 400)
+    }
+    else {
+        next()
+    }
+    //console.log(result)
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressErrors(msg, 400)
+    }
+    else {
+        next()
+    }
+    //console.log(result)
+}
+
 app.get("/", (req, res) => {
     //res.send("Welcome to home page");
     res.render("home")
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', {campgrounds})
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post('/campgrounds', validateCampground, catchAsync(async(req, res, next) => {
+    // if (!req.body.campground) throw new ExpressErrors('OLya so,e dffse', 404)
+    
+        const campground = new Campground(req.body.campground)
+        await campground.save()
+        res.redirect(`/campgrounds/${campground._id}`)
+}
+)
+)
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const camp = await Campground.findById(req.params.id)
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
+    const camp = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', {camp})
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const camp = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', {camp})
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
-})
+}))
 
+app.post('/campgrounds/:id/review', validateReview, catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
+app.delete('/campgrounds/:id/review/:reviewId', catchAsync(async (req, res, next) => {
+    const { id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 // app.get('/makecampground', async (req, res) => {
 //     const camp = new Campground({title: 'My House', description: 'Nice weather' })
 //     await camp.save()
 //     res.send(camp)
 // })
+
+app.all("*", (req, res, next) => {
+    next(new ExpressErrors('Page not found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const {status = 500} = err;
+    if (!err.message) err.message = 'Oh no what is thu'
+    res.status(status).render('error', {err});
+})
 
 app.listen(3000, () => {
     console.log("YelpCamp Connected")
---
+++ utils/expressErrors.js
@@ -0,0 +1,9 @@
+class ExpressErrors extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+module.exports = ExpressErrors;
